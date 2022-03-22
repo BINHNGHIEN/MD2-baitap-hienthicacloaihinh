faeb0089df0c83ad4d3ff6db3100343ae53101e6
BINHNGHIEN <haibinh.94.bui@gmail.com>
2022-03-22 10:37:31 +0700

    noidung

---
+++ src/index.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class index {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("");
+            for (int j = 0; j <7; j++) {
+                System.out.print(" * ");
+            }
+        }
+    }
+}
